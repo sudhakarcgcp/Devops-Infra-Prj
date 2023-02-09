bfccc8891a0a8cb3570b6fcdab45d56951a0abd3
sudhakarcgcp <sudhakarcgcp@gmail.com>
2023-02-09 19:18:37 +0000

    sonar instance addition

---
+++ sonar.tf
@@ -0,0 +1,51 @@
+resource "aws_vpc" "sonar"
+{
+ cidr_block = "172.31.0.0/16"
+ instance_tenancy = "default"
+ tags = {
+   Name = "Devops-Prj"
+  }
+}
+
+resource "aws_security_group" "Devsecgrp-sonar"
+{
+ Name = "Devsecgrp-sonar"
+ description = "Security group for sonarqube server"
+ ingress
+	{
+	from_port = 9000
+	to_port   = 9000
+	protocol  = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	}
+ ingress
+	{
+	from_port = 22
+	to_port   = 22
+	protocol  = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	}
+ egress
+	{
+	from_port = 0
+	to_port   = 65535
+	protocol  = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	}
+	
+ tags =
+	{
+	Name = "SonarSG"
+	}
+
+resourcce "aws_instance" "sonar_instance"
+	{
+	ami= var.ami_id
+	key_name = var.key_name
+	instance_type = var.instance_type
+	vpc_security_group_ids = [aws_security_group.SonarSG.id]
+	tags= {
+	Name = var.tag_name
+	}
+}
+
