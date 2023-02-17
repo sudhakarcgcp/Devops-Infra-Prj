e984f1985e97ddb47e01d4d1cbffeda097f28560
sudhakarcgcp <sudhakarcgcp@gmail.com>
2023-02-17 11:42:29 +0000

    adding new files

--- main.tf
+++ main.tf
@@ -55,3 +55,36 @@ resource "aws_instance" "myFirstInstance" {
     Name = var.tag_name
   }
 }
+
+#1 -this will create a S3 bucket in AWS
+resource "aws_s3_bucket" "terraform_state_s3" {
+  #make sure you give unique bucket name
+  bucket = "infraprj-tfstate-bucket"
+  force_destroy = true
+# Enable versioning to see full revision history of our state files
+  versioning {
+         enabled = true
+        }
+
+# Enable server-side encryption by default
+server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# 2 - this Creates Dynamo Table
+resource "aws_dynamodb_table" "terraform_locks" {
+# Give unique name for dynamo table name
+  name         = "infraprj-tfstatelock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+        attribute {
+         name = "LockID"
+         type = "S"
+      }
+}
+
